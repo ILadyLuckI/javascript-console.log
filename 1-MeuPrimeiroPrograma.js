b19975677c535196000502b9acbcfd42199d0ad2
Daiana Koblitz <69736274+ILadyLuckI@users.noreply.github.com>
2023-03-21 15:50:31 -0300

    Arquivo adicionado via upload

---
+++ 1-MeuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu Primeiro Programa: trabalhando com variáveis");
+
+const idade = 34;
+
+console.log("idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade * 2);
+console.log(idade / 2);
+
+const idadeSomada = idade + 2;
+console.log("Minha idade somada + 2 é: " + idadeSomada);
